a8ed1ad51a7428aa7e39131c0364b5c7d14e7e92
sujin <mchead1@naver.com>
2017-02-11 12:50:11 +0900

    getUrlSource 클래스 선언

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * Created by Cheong on 2/11/2017.
  */
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        getUrlSource url = new getUrlSource();
+    }
+    private static String getUrlSource(String url) throws IOException {
+        URL yahoo = new URL(url);
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                yc.getInputStream(), "UTF-8"));
+        String inputLine;
+        StringBuilder a = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            a.append(inputLine);
+        in.close();
+
+        return a.toString();
     }
 }
