56f57fcc4b99cf1417d96af3261d06abf620525f
Cheonghyun Lee <lee@cheonghyun.com>
2017-02-11 12:59:26 +0900

    네이버 뉴스 코드 받아오는 부분 구현

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,14 +9,31 @@ import java.net.URLConnection;
  */
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        getUrlSource url = new getUrlSource();
+    /**
+     * 메인 함수
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+
+        String htmlCode;
+
+        htmlCode = getUrlSource("http://news.naver.com/");
+
+        System.out.println(htmlCode);
     }
+
+    /**
+     * html 소스 받아오는 코드
+     * http://stackoverflow.com/questions/8616781/how-to-get-a-web-pages-source-code-from-java
+     * @param url 소스 받아올 웹주소
+     * @return html 코드
+     * @throws IOException
+     */
     private static String getUrlSource(String url) throws IOException {
         URL yahoo = new URL(url);
         URLConnection yc = yahoo.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(
-                yc.getInputStream(), "UTF-8"));
+                yc.getInputStream(), "euc-kr")); // 인코딩을 euc-kr로 변경
         String inputLine;
         StringBuilder a = new StringBuilder();
         while ((inputLine = in.readLine()) != null)
