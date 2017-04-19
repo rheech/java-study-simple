053e8cd7a98277241f06628c0c73b49a059defbb
Cheonghyun Lee <lee@cheonghyun.com>
2017-04-19 17:14:36 +0900

    ParseException 오류 수정

--- src/PriorityQueueTest.java
+++ src/PriorityQueueTest.java
@@ -2,6 +2,8 @@ import java.text.*;
 import java.util.*;
 import java.io.*;
 class PriorityQueueTest {
+    // 1. throws 문 선언하거나
+    // public static void main(String[] args) throws Exception { 으로 써도 됨
     public static void main(String[] args) {
         String[] data = {
                 "INSERT INTO CUST_INFO VALUES ('이자바','02-123-1234','27','07-09');",
@@ -12,7 +14,13 @@ class PriorityQueueTest {
 
         MessageFormat mf = new MessageFormat(pattern);
 
-        for(int i =0; i<data.length; i++)
-        Object[] r = mf.parse(data[i]);
+        // 2. 혹은 try catch 문으로 감싸도 됨
+        try {
+            for (int i = 0; i < data.length; i++) {
+                Object[] r = mf.parse(data[i]);
+            }
+        } catch (ParseException pex) {
+            pex.printStackTrace();
+        }
     }
 }
